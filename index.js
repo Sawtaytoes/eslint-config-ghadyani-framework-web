701c0e71a828bfea2f06a0b2e56a8552cf3bc7ff
Kevin Ghadyani <Saturn2888@gmail.com>
2018-07-22 19:11:45 -0500

    Added files

---
+++ index.js
@@ -0,0 +1,37 @@
+module.exports = {
+	env: {
+		browser: true,
+	},
+	extends: [
+		'ghadyani-framework-base',
+		'plugin:react/recommended',
+	],
+	parser: 'babel-eslint',
+	parserOptions: {
+		'ecmaFeatures': {
+			jsx: true,
+			modules: true,
+		},
+	},
+	rules: {
+		'compat/compat': 'warn',
+		'jsx-quotes': 'error',
+		'no-multi-spaces': 'error',
+		'react/jsx-boolean-value': 'error',
+		'react/jsx-closing-bracket-location': 'error',
+		'react/jsx-closing-tag-location': 'error',
+		'react/jsx-curly-spacing': 'error',
+		'react/jsx-pascal-case': 'error',
+		'react/jsx-tag-spacing': 'error',
+		'react/jsx-uses-react': 'warn',
+		'react/jsx-uses-vars': 'warn',
+		'react/no-string-refs': 'error',
+		'react/prop-types': 'warn',
+		'react/react-in-jsx-scope': 'warn',
+		'react/self-closing-comp': 'error',
+	},
+	plugins: [
+		'compat',
+		'react',
+	],
+}
